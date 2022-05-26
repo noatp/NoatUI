b7aee4993c2e3101760af14c546128526fdf23f2
Toan Pham <toan.chpham@gmail.com>
2022-05-26 07:03:28 -0700

    added vstack and hstack

---
+++ Sources/NoatUI/Extension/UIStackView.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Toan Pham on 5/25/22.
+//
+
+import Foundation
+import UIKit
+
+
--- Sources/NoatUI/Extension/UIView.swift
+++ Sources/NoatUI/Extension/UIView.swift
@@ -24,7 +24,7 @@ public extension UIView{
         }
         
         if let bottom = bottom {
-            bottomAnchor.constraint(equalTo: bottom, constant: padding.bottom).isActive = true
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
         }
         
         if let leading = leading {
@@ -32,7 +32,7 @@ public extension UIView{
         }
         
         if let trailing = trailing {
-            trailingAnchor.constraint(equalTo: trailing, constant: padding.right).isActive = true
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
         }
         
         if size.width != 0 {
@@ -42,5 +42,31 @@ public extension UIView{
         if size.height != 0{
             heightAnchor.constraint(equalToConstant: size.height).isActive = true
         }
+    }
+    
+    func vstack(
+        alignment: UIStackView.Alignment = .fill,
+        spacing: CGFloat = .zero,
+        subviews: [UIView]
+    ) -> UIStackView {
+        let stackView = UIStackView.init(arrangedSubviews: subviews)
+        stackView.axis = .vertical
+        stackView.alignment = alignment
+        stackView.spacing = spacing
+        self.addSubview(stackView)
+        return stackView
+    }
+    
+    func hstack(
+        alignment: UIStackView.Alignment = .fill,
+        spacing: CGFloat = .zero,
+        subviews: [UIView]
+    ) -> UIStackView {
+        let stackView = UIStackView.init(arrangedSubviews: subviews)
+        stackView.axis = .horizontal
+        stackView.alignment = alignment
+        stackView.spacing = spacing
+        self.addSubview(stackView)
+        return stackView
     }
 }
